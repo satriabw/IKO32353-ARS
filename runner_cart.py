b61999129fc9cea1c55a435d8f82d024c53d0b1a
Satria Bagus <satria_bagus@hotmail.com>
2018-04-20 19:17:37 +0700

    Fix runner

--- runner_cart.py
+++ runner_cart.py
@@ -1,8 +1,11 @@
 from decision_tree import RegressionTree
 import numpy as np
 
-my_data = np.genfromtxt('Train_Datasets.csv', delimiter=',', usecols=(1, 3, 5, 7, -1), skip_header=1)
+# Get data from train dataset
+my_data = np.genfromtxt('Train_Dataset_Mini.csv', delimiter=',', usecols=(1, 3, 5, 7, -1), skip_header=1)
 X, y = my_data[:, :-1], my_data[:, -1]
 
 models = RegressionTree()
 models.fit(X, y)
+# Test predict for some input
+print(models.predict(np.array([[11.65, 0.040081193, 227.0694, 2009]])), 1141.847)
