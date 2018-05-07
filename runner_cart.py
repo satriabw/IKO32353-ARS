4442739938f4d675a2f0dcbf67f677967713d545
Rizal Diantoro <rizaldntr@gmail.com>
2018-05-07 12:46:31 +0700

    add all column to predictor

--- runner_cart.py
+++ runner_cart.py
@@ -6,7 +6,7 @@ from decision_tree import RegressionTree
 
 # Get data from train dataset
 my_data = np.genfromtxt('Train_Dataset_Mini.csv', delimiter=',',
-                        usecols=(1, 3, 5, 7, -1), skip_header=1)
+                        skip_header=1, dtype=None, encoding=None)
 X, y = my_data[:, :-1], my_data[:, -1]
 
 models = RegressionTree()
