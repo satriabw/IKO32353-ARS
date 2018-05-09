b10d1049986813fe85e7d663842d23c8c29dc3ed
Satria Bagus <satria_bagus@hotmail.com>
2018-05-09 18:59:44 +0700

    merge

--- runner_cart.py
+++ runner_cart.py
@@ -51,29 +51,29 @@ class DatasetTransformer():
 # my_data = my_data.values
 # X, y = my_data[:, :-1], my_data[:, -1]
 
-my_data = np.genfromtxt('Train_Datasets.csv', delimiter=',',
-                        skip_header=1, dtype=None, encoding=None)
+# my_data = np.genfromtxt('Train_Datasets.csv', delimiter=',',
+#                         skip_header=1, dtype=None, encoding=None)
 
-X = np.zeros(shape=(1, 11))
-y = np.zeros(shape=(1, 1))
-for data in my_data:
-    raw = list(data)
-    data = np.array([raw[:-1]]) 
-    target = np.array([raw[-1]])
-    X = np.vstack((data, X))
-    y = np.vstack((target, y))
+# X = np.zeros(shape=(1, 11))
+# y = np.zeros(shape=(1, 1))
+# for data in my_data:
+#     raw = list(data)
+#     data = np.array([raw[:-1]]) 
+#     target = np.array([raw[-1]])
+#     X = np.vstack((data, X))
+#     y = np.vstack((target, y))
 
 # X, y = my_data[:, :-1], my_data[:, -1]
 
 # print(X)
 # print(y)
-models = RegressionTree()
-X = X[:-1]
-y = y[:-1]
-models.fit(X, y)
+# models = RegressionTree()
+# X = X[:-1]
+# y = y[:-1]
+# models.fit(X, y)
 
 filename = 'finalized_model.sav'
-pickle.dump(models, open(filename, 'wb'))
+# pickle.dump(models, open(filename, 'wb'))
 
 loaded_model = pickle.load(open(filename, 'rb'))
 
@@ -84,7 +84,7 @@ loaded_model = pickle.load(open(filename, 'rb'))
 
 args = sys.argv[1:]
 if len(args) < 4:
-    args = ['FDV11',9.1,'Regular',0,'Breads',173.2054,'OUT045',2002,'Medium','Tier 2','Supermarket Type1']
+    args = ['FDV11',9.1,'Regular',0,'Breads',173.2054,'OUT045',1997,'Medium','Tier 2','Supermarket Type1']
 
 print(loaded_model.predict(np.array([args])),
       1141.847)
