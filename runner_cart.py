4553e0c932818d09c723a88043ba1c7704463d28
Rizal Diantoro <rizaldntr@gmail.com>
2018-05-09 13:40:47 +0700

    save model to pickle

--- runner_cart.py
+++ runner_cart.py
@@ -1,11 +1,11 @@
 import sys
-
+import pickle
 import numpy as np
 
 from decision_tree import RegressionTree
 
 # Get data from train dataset
-my_data = np.genfromtxt('Train_Dataset_Mini.csv', delimiter=',',
+my_data = np.genfromtxt('Train_Datasets.csv', delimiter=',',
                         skip_header=1, dtype=None, encoding=None)
 
 X = np.zeros(shape=(1, 11))
@@ -23,10 +23,16 @@ models = RegressionTree()
 X = X[:-1]
 y = y[:-1]
 models.fit(X, y)
+
+filename = 'finalized_model.sav'
+pickle.dump(models, open(filename, 'wb'))
+
+loaded_model = pickle.load(open(filename, 'rb'))
+
 # Test predict for some input
 args = sys.argv[1:]
 if len(args) < 4:
     args = ['FDV11',9.1,'Regular',0,'Breads',173.2054,'OUT045',2002,'Medium','Tier 2','Supermarket Type1']
 
-print(models.predict(np.array([args])),
+print(loaded_model.predict(np.array([args])),
       1141.847)
