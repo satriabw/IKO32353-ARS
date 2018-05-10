436e656b2f9ba51a0100483bc296c6372749a92a
Satria Bagus <satria_bagus@hotmail.com>
2018-05-10 16:11:54 +0700

    Add update in trainer.py

--- runner_cart.py
+++ runner_cart.py
@@ -5,78 +5,31 @@ import pandas as pd
 
 from decision_tree import RegressionTree
 
-
-
 # Get data from train dataset
-# my_data = np.genfromtxt('Train_Dataset_Mini.csv', delimiter=',', skip_header=1)
+my_data = np.genfromtxt('./datasets/Train_Datasets.csv', delimiter=',', skip_header=1, dtype=None, encoding=None)
+X = np.zeros(shape=(1, 11))
+y = np.zeros(shape=(1, 1))
+for data in my_data:
+    raw = list(data)
+    data = np.array([raw[:-1]]) 
+    target = np.array([raw[-1]])
+    X = np.vstack((data, X))
+    y = np.vstack((target, y))
 
-
-
-# my_data = np.genfromtxt('Train_Datasets.csv', delimiter=',',
-#                         skip_header=1, dtype=None, encoding=None)
-
-# X = np.zeros(shape=(1, 11))
-# y = np.zeros(shape=(1, 1))
-# for data in my_data:
-#     raw = list(data)
-#     data = np.array([raw[:-1]]) 
-#     target = np.array([raw[-1]])
-#     X = np.vstack((data, X))
-#     y = np.vstack((target, y))
-
-# X, y = my_data[:, :-1], my_data[:, -1]
-
-# print(X)
-# print(y)
+X, y = my_data[:, :-1], my_data[:, -1]
 models = RegressionTree()
-# # X = X[:-1]
-# # y = y[:-1]
+# X = X[:-1]
+# y = y[:-1]
 models.fit(X, y)
 
-filename = 'finalized_model.sav'
+filename = 'models_version_one.sav'
 pickle.dump(models, open(filename, 'wb'))
 
-# loaded_model = pickle.load(open(filename, 'rb'))
+loaded_model = pickle.load(open(filename, 'rb'))
 
 # Test predict for some input
-# args = sys.argv[1:]
-# # if len(args) < 4:
-#     args = [11.65, 0.040081193, 227.0694, 2009]
-
-
-# args = sys.argv[1:]
-# if len(args) < 4:
-#     args = ['FDT07',5.82,'reg',0,'Fruits and Vegetables',256.633,'OUT049',1999,'Medium','Tier 1','Supermarket Type1']
-
-new_args = []
-# i = 0
-# for el in args:
-#     if i == 0:
-#         item = maps['Item_Identifier'].get(el)
-#         new_args.append(item)
-#     elif i == 2:
-#         item = maps['Item_Fat_Content'].get(el)
-#         new_args.append(item)
-#     elif i == 4:
-#         item = maps['Item_Type'].get(el)
-#         new_args.append(item)
-#     elif i == 6:
-#         item = maps['Outlet_Identifier'].get(el)
-#         new_args.append(item)
-#     elif i == 8:
-#         item = maps['Outlet_Size'].get(el)
-#         new_args.append(item)
-#     elif i == 9:
-#         item = maps['Outlet_Location_Type'].get(el)
-#         new_args.append(item)
-#     elif i == 10:
-#         item = maps['Outlet_Type'].get(el)
-#         new_args.append(item)
-#     else:
-#         new_args.append(args[i])
-#     i += 1
-
-# print(new_args)
-
-# print(loaded_model.predict(np.array([new_args])),
-#       2050.664)
+args = sys.argv[1:]
+if len(args) < 4:
+    args = ['FDT07',5.82,'reg',0,'Fruits and Vegetables',256.633,'OUT049',1999,'Medium','Tier 1','Supermarket Type1']
+print(loaded_model.predict(np.array([args])),
+      2050.664)
--- trainer.py
+++ trainer.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from decision_tree import RegressionTree
 from sklearn import metrics
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
 
 class DatasetTransformer():
     def __init__(self, dataset, features):
@@ -39,25 +40,65 @@ class DatasetTransformer():
         dataset, maps = self._transformCategorical(self.dataset, self.features)
         return dataset, maps
 
-
-
 # Get data from train dataset
-my_data = pd.read_csv('Test_Dataset_Cleansed.csv')
-features = ['Item_Identifier', 'Item_Fat_Content', 'Item_Type', 'Outlet_Identifier', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type']
+my_data = pd.read_csv('./datasets/Datasets_Cleaned.csv')
+msk = np.random.rand(len(my_data)) < 0.8
 
-transformer = DatasetTransformer(my_data, features)
-my_data, maps = transformer.transform()
+size = my_data.shape[1]
 
-X = my_data.values
+train = my_data[msk].values
+test = my_data[~msk].values
+X_train, y_train = train[:, 1:-1], train[:, -1]
+X_test, y_test = test[:, 1:-1], test[:, -1]
 
-# models = DecisionTreeRegressor()
-# models.fit(X_train, y_train)
+
+models_dtreg = DecisionTreeRegressor()
+models_dtreg.fit(X_train, y_train)
+
+models_svr = SVR(kernel='rbf', C=1e3, gamma=0.1)
+models_svr.fit(X_train, y_train)
+
+filename = 'models_finals.sav'
+# pickle.dump(models, open(filename, 'wb'))
+models_cart = pickle.load(open(filename, 'rb'))
+
+y_pred_cart = models_cart.predict(X_test)
+y_pred_dtreg = models_dtreg.predict(X_test)
+y_pred_svr = models_svr.predict(X_test)
+
+print("Decision Tree Regressor: ", np.sqrt(metrics.mean_squared_error(y_pred_dtreg, y_test)))
+print("Support Vector Machine Regressor: ", np.sqrt(metrics.mean_squared_error(y_pred_svr, y_test)))
+print("Our Models: ", np.sqrt(metrics.mean_squared_error(y_pred_cart, y_test)))
 
 
-filename = 'finalized_model.sav'
-# # pickle.dump(models, open(filename, 'wb'))
-models = pickle.load(open(filename, 'rb'))
-y_pred = models.predict(X)
 
-my_data["Item_Outlet_Sales"] = y_pred
-my_data.to_csv('jawaban.csv')
+
+# new_args = []
+# i = 0
+# for el in args:
+#     if i == 0:
+#         item = maps['Item_Identifier'].get(el)
+#         new_args.append(item)
+#     elif i == 2:
+#         item = maps['Item_Fat_Content'].get(el)
+#         new_args.append(item)
+#     elif i == 4:
+#         item = maps['Item_Type'].get(el)
+#         new_args.append(item)
+#     elif i == 6:
+#         item = maps['Outlet_Identifier'].get(el)
+#         new_args.append(item)
+#     elif i == 8:
+#         item = maps['Outlet_Size'].get(el)
+#         new_args.append(item)
+#     elif i == 9:
+#         item = maps['Outlet_Location_Type'].get(el)
+#         new_args.append(item)
+#     elif i == 10:
+#         item = maps['Outlet_Type'].get(el)
+#         new_args.append(item)
+#     else:
+#         new_args.append(args[i])
+#     i += 1
+
+# print(new_args)
